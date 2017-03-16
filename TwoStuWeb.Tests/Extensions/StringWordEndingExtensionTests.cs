42ae0b6411c0f9ae62c73fb05b09f6663df1676c
Дмитрий Сердюков <dimaserd84@gmail.com>
2017-03-16 16:37:50 +0300

    jbkh

---
+++ TwoStuWeb.Tests/Extensions/StringWordEndingExtensionTests.cs
@@ -0,0 +1,32 @@
+﻿using Extensions.String;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TwoStuWeb.Tests.Extensions
+{
+    [TestClass]
+    public class StringWordEndingExtensionTests
+    {
+        [TestMethod]
+        public void OnlyUniqueEndings()
+        {
+            bool result = false;
+            string duplicate = string.Empty;
+
+            List<string> endings = WordEndingStringExtensions.AllWordEndings.ToList();
+
+            foreach(string ending in endings)
+            {
+                if(endings.Count(x => x == ending) > 1)
+                {
+                    result = true;
+                    duplicate = ending;
+                }
+            }
+
+            Assert.AreEqual(string.Empty, duplicate);
+            Assert.AreEqual(false, result);
+        }
+    }
+}
